d5d35d14bea4ffc0555c63362cbb505de5f6e3bc
Phong Ngo <phong.ngo@servian.com>
2023-06-01 16:02:26 +1000

    Initial commit

---
+++ MVVM-Coordinator/MVVM-Coordinator/MVVM_CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_CoordinatorApp.swift
+//  MVVM-Coordinator
+//
+//  Created by Phu Phong Ngo on 1/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
